99705b82e215d634b905234aaca358fa4be3436b
Kristina Skåre <kristina.skare@helse-bergen.no>
2021-01-21 15:41:03 +0100

    utils med nyttigeoperatorer

---
+++ R/utils.R
@@ -0,0 +1,47 @@
+#' Pipe operator
+#'
+#' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
+
+#' Compound assignment pipe operator
+#'
+#' See \code{magrittr::\link[magrittr:pipe]{\%<>\%}} for details.
+#'
+#' @name %<>%
+#' @rdname compound_assignment_pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %<>%
+#' @usage lhs \%<>\% rhs
+NULL
+
+#' Exposition pipe operator
+#'
+#' See \code{magrittr::\link[magrittr:pipe]{\%$\%}} for details.
+#'
+#' @name %$%
+#' @rdname exposition_pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %$%
+#' @usage lhs \%$\% rhs
+NULL
+
+#' Data pronouns
+#'
+#' See \code{rlang::\link[rlang:tidyeval-data]{.data}} for details
+#'
+#' @name .data
+#' @rdname pronoun
+#' @keywords internal
+#' @export
+#' @importFrom rlang .data
+#' @usage .data
+NULL
