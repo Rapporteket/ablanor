1801e39f0c5bd3e99e4e455e07cc51f304a1adcb
Kristina Skåre <kristina.skare@helse-bergen.no>
2021-01-21 15:56:33 +0100

    funksjonar for å rekne ut kvalitetsindikatorar

---
+++ tests/testthat/test-kvalitetsindikatorar.R
@@ -0,0 +1,5 @@
+test_that("Teste kvalitestindikatorer", {
+  # Teste at variablene ki_krit_teller og ki_krit_nevner blir laget
+  # teste at dim(df ut) har to kolonner mer enn dim(df inn)
+  # teste format data.frame
+})
