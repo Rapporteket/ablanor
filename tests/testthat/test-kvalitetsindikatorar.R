825a0240634182ef7d5c51008551581c86659bf0
Kristina Skåre <kristina.skare@helse-bergen.no>
2021-01-22 10:11:31 +0100

    Tester for funksjonen ki_komplikasjonar()

--- tests/testthat/test-kvalitetsindikatorar.R
+++ tests/testthat/test-kvalitetsindikatorar.R
@@ -1,5 +1,13 @@
-test_that("Teste kvalitestindikatorer", {
-  # Teste at variablene ki_krit_teller og ki_krit_nevner blir laget
-  # teste at dim(df ut) har to kolonner mer enn dim(df inn)
-  # teste format data.frame
+test_that("Teste kvalitestindikatorer - Ki_komplikajsoner", {
+  x <- data.frame(komp_janei = c(0,0,0,1,0,1, 2, NA))
+
+  expect_equal(dim(ki_komplikasjonar(x)),
+               c(8,3))
+  expect_equal(colnames(ki_komplikasjonar(x)),
+               c("komp_janei", "ki_krit_teller", "ki_krit_nevner"))
+  expect_true(is.na(ki_komplikasjonar(x)$ki_krit_teller[8]))
+  expect_true(sum(ki_komplikasjonar(x)$ki_krit_teller, na.rm = TRUE) == 2)
+  expect_true(sum(ki_komplikasjonar(x)$ki_krit_nevner, na.rm = TRUE) == 6)
+  expect_true(class(ki_komplikasjonar(x)) == "data.frame")
+
 })
