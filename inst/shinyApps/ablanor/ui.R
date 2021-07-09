26d9c24de4d033184c8aecddc1becadb6d281850
Are Edvardsen <biorakel@gmail.com>
2021-07-09 10:25:35 +0200

    explicit fun refs

--- inst/shinyApps/ablanor/ui.R
+++ inst/shinyApps/ablanor/ui.R
@@ -1,42 +1,27 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# shiny web app ui
 
-library(shiny)
-library(ablanor)
-library(rapbase)
-library(raplog)
-library(magrittr)
-library(rpivotTable)
-
-addResourcePath("rap", system.file("www", package = "rapbase"))
+shiny::addResourcePath("rap", system.file("www", package = "rapbase"))
 reg_title <- "AblaNor"
 
 
-
-ui <- tagList(
-  navbarPage(
-    title = div(a(includeHTML(system.file("www/logo.svg",
+ui <- shiny::tagList(
+  shiny::navbarPage(
+    title = shiny::div(shiny::a(shiny::includeHTML(system.file("www/logo.svg",
                                           package = "rapbase"))),
                 reg_title),
     windowTitle = reg_title,
     theme = "rap/bootstrap.css",
     id = "tabs",
 
-    tabPanel("Start",
+    shiny::tabPanel("Start",
              shinyalert::useShinyalert(),
              mainPanel(width = 12,
-                       htmlOutput("veiledning", inline = TRUE),
-                       appNavbarUserWidget(user = uiOutput("appUserName"),
+                       shiny::htmlOutput("veiledning", inline = TRUE),
+                       rapbase::appNavbarUserWidget(user = uiOutput("appUserName"),
                                            organization =
                                              uiOutput("appOrgName"),
                                            addUserInfo = TRUE),
-                       tags$head(tags$link(rel = "shortcut icon",
+                       shiny::tags$head(shiny::tags$link(rel = "shortcut icon",
                                            href = "rap/favicon.ico"))
              )
 
@@ -44,15 +29,15 @@ ui <- tagList(
 
 
 
-    tabPanel("Utforsker",
-             fluidRow(
+    shiny::tabPanel("Utforsker",
+             shiny::fluidRow(
                column(6, uiOutput("selectDataSet")),
                column(6, uiOutput("selectVars"))
              ),
-             fluidRow(
+             shiny::fluidRow(
                column(12, uiOutput("togglePivotSurvey"))
              ),
-             fluidRow(
+             shiny::fluidRow(
                column(12,
                       rpivotTableOutput("pivotSurvey"))
              )
@@ -61,46 +46,46 @@ ui <- tagList(
 
 
 
-    tabPanel("Datadump",
+    shiny::tabPanel("Datadump",
              # ADD SOME CONTENT, Should we downoad an already controlled
              # datadump using les_data_ablanor()?
     ),
 
-    tabPanel("Månedsrapporter",
-             sidebarLayout(
-               sidebarPanel(
-                 radioButtons("formatReport",
+    shiny::tabPanel("Månedsrapporter",
+             shiny::sidebarLayout(
+               shiny::sidebarPanel(
+                 shiny::radioButtons("formatReport",
                               "Format for nedlasting",
                               c("PDF", "HTML"),
                               inline = FALSE),
-                 downloadButton("downloadReport", "Last ned!"),
+                 shiny::downloadButton("downloadReport", "Last ned!"),
                  width = 2
                ),
-               mainPanel(
-                 htmlOutput("maanedligRapport", inline = TRUE)
+               shiny::mainPanel(
+                 shiny::htmlOutput("maanedligRapport", inline = TRUE)
                )
 
              )
     ),
 
 
-    tabPanel("Abonnement",
-             sidebarLayout(
-               sidebarPanel(width = 3,
-                            uiOutput("subscriptionRepList"),
-                            selectInput("subscriptionFreq", "Frekvens:",
+    shiny::tabPanel("Abonnement",
+             shiny::sidebarLayout(
+               shiny::sidebarPanel(width = 3,
+                            shiny::uiOutput("subscriptionRepList"),
+                            shiny::selectInput("subscriptionFreq", "Frekvens:",
                                         list(Årlig = "Årlig-year",
                                               Kvartalsvis = "Kvartalsvis-quarter",
                                               Månedlig = "Månedlig-month",
                                               Ukentlig = "Ukentlig-week",
                                               Daglig = "Daglig-DSTday"),
                                         selected = "Månedlig-month"),
-                            selectInput("subscriptionFileFormat", "Format:",
+                            shiny::selectInput("subscriptionFileFormat", "Format:",
                                         c("html", "pdf")),
-                            actionButton("subscribe", "Bestill!")
+                            shiny::actionButton("subscribe", "Bestill!")
                ),
-               mainPanel(
-                 uiOutput("subscriptionContent")
+               shiny::mainPanel(
+                 shiny::uiOutput("subscriptionContent")
                )
              )
     )
