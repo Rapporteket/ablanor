33c276c9dae8c266a319611600809b24dcf97be7
Kristina Skåre <kristina.skare@helse-bergen.no>
2021-10-18 15:39:00 +0200

    Legg til oppfølgingddata i utforsker

--- R/getProsPatientData.R
+++ R/getProsPatientData.R
@@ -22,6 +22,7 @@ getProsPatientData <- function(registryName,
   d_pros <- d$pros
   d_mce <- d$mce
   d_patientlist <- d$patientlist
+  d_followup <- d$followup
 
   ## BEHANDLING AV DATABASEN I R:
   # FELLES VARIABEL-NAVN I TO TABELLER (status for skjema etc)
@@ -51,18 +52,56 @@ getProsPatientData <- function(registryName,
   # finst vert tekne med.
 
   d_ablanor <- d_pros %>%
-    dplyr::left_join(., d_mce %>% dplyr::select(.data$MCEID,
-                                                .data$PATIENT_ID,
-                                                .data$STATUS),
+    dplyr::left_join(.,
+                     d_mce %>% dplyr::select(.data$MCEID,
+                                             .data$PATIENT_ID,
+                                             .data$STATUS),
                      by = "MCEID") %>%
-    dplyr::left_join(., d_patientlist,
+    dplyr::left_join(.,
+                     d_patientlist,
                      by = c("PATIENT_ID" = "ID")) %>%
-    dplyr::left_join(., d_basereg, by = c("MCEID", "CENTREID"))
+    dplyr::left_join(.,
+                     d_basereg,
+                     by = c("MCEID", "CENTREID"))
 
   # Sjekk at ingen variabel-navn teller dobbelt.
   # TEST I getLocalProsedyrePasientData
   # d_ablanor %>% dplyr::select(ends_with(".x") | ends_with(".y"))
 
+
+  # Forberede Followup-data
+  followup_data <- d_followup %>%
+    dplyr::rename("MCEID_FOLLOWUP" = .data$MCEID) %>%
+    dplyr::rename_at(dplyr::vars(.data$USERCOMMENT:.data$CREATEDBY),
+                     function(x) {
+                       paste0("followup_", x)
+                     }) %>%
+    # Kobler med alle forløp av type = 9, for å legge til parentmceid.
+    # (Parentmceid er forløpet som ligger til grunn for utsending av oppf.)
+    dplyr::left_join(.,
+                     d_mce %>%
+                       dplyr::filter(.data$MCETYPE == 9) %>%
+                       dplyr::select(.data$MCEID, .data$PARENTMCEID) %>%
+                       dplyr::rename("MCEID_FOLLOWUP" = .data$MCEID,
+                                     "MCEID" = .data$PARENTMCEID),
+                     by = "MCEID_FOLLOWUP") %>%
+    dplyr::relocate(.data$MCEID, .before = "MCEID_FOLLOWUP")
+
+  # Sjekk at bare en oppfølging per forløp
+  # (I starten ble flere skjema sendt ut da er det nyeste skjema som gjelder)
+  followup_data %<>%
+    dplyr::group_by(.data$MCEID) %>%
+    dplyr::mutate(max_mceid_followup = max(.data$MCEID_FOLLOWUP)) %>%
+    dplyr::ungroup() %>%
+    dplyr::filter(.data$MCEID_FOLLOWUP == .data$max_mceid_followup) %>%
+    dplyr::select(- .data$max_mceid_followup)
+
+
+  # Legg til follow-up i pasient - prosedyre - data
+  d_ablanor %<>%
+    dplyr::left_join(.,
+                     followup_data,
+                     by = c("MCEID", "CENTREID"))
 
 
   names(d_ablanor) <- tolower(names(d_ablanor))
@@ -71,12 +110,12 @@ getProsPatientData <- function(registryName,
   # UTLEDETE VARIABLER
 
   # ALDER :
- d_ablanor %<>%
-   ablanor::utlede_alder(.) %>%
-   ablanor::utlede_alder_75(.) %>%
-   ablanor::utlede_aldersklasse(.)
+  d_ablanor %<>%
+    ablanor::utlede_alder(.) %>%
+    ablanor::utlede_alder_75(.) %>%
+    ablanor::utlede_aldersklasse(.)
 
- # BMI klasse
+  # BMI klasse
   d_ablanor %<>%
     abalnor::utlede_bmi_klasse(.)
 
