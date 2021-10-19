d3afb2bf69675f85a5e2d4431e5dd2f00303638e
Kristina Skåre <kristina.skare@helse-bergen.no>
2021-10-19 13:45:12 +0200

    Sykehusnavn i tekst-streng

---
+++ tests/testthat/test-legg_til_sykehusnavn.R
@@ -0,0 +1,43 @@
+test_that("legg til sykehusnavn works", {
+
+  df <- data.frame(centreid = c(102966, 104284, 4214492, 700328, 700728,
+                                NA, 123456))
+
+
+  # Navn ny kolonne
+  testthat::expect_equal(
+    ablanor::legg_til_sykehusnavn(df) %>% names(),
+    c("centreid", "sykehusnavn")
+  )
+
+  # FOrventer Sykehusnavn = NA dersom feil centreid
+  testthat::expect_true(all(
+    ablanor::legg_til_sykehusnavn(df) %>%
+      dplyr::filter(is.na(.data$centreid) | .data$centreid == 123456) %>%
+      dplyr::pull(.data$sykehusnavn) %>%
+      is.na()))
+
+  # FOrventer disse navnene (short)
+  testthat::expect_equal(
+    ablanor::legg_til_sykehusnavn(df, short = TRUE) %>%
+      dplyr::filter(! is.na(.data$centreid) & .data$centreid != 123456) %>%
+      dplyr::pull(.data$sykehusnavn),
+    c("HUS", "St.Olavs", "LHL", "OUS", "UNN"))
+
+  # FOrventer disse navnene (long)
+  testthat::expect_equal(
+    ablanor::legg_til_sykehusnavn(df, short = FALSE) %>%
+      dplyr::filter(! is.na(.data$centreid) & .data$centreid != 123456) %>%
+      dplyr::pull(.data$sykehusnavn),
+    c("Haukeland Universitetssykehus", "St.Olavs Hospital", "LHL Gardermoen",
+      "Oslo Universitetssykehus", "Universitetssykehuset Nord-Norge"))
+
+
+   # Mangler centreid - error
+  testthat::expect_error(ablanor::legg_til_sykehusnavn(df = data.frame(a = 1)))
+
+   # Mangler short er annet enn true/false
+  testthat::expect_error(
+    ablanor::legg_til_sykehusnavn(df = df, short = data.frame(a = 1)))
+
+})
