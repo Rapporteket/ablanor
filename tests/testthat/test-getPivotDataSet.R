8f592e26981c7ca1edb482ab5d8524410318edf5
Are Edvardsen <biorakel@gmail.com>
2021-10-22 09:04:19 +0000

    test that function requires a reshId on data subset

--- tests/testthat/test-getPivotDataSet.R
+++ tests/testthat/test-getPivotDataSet.R
@@ -1,3 +1,14 @@
 test_that("Function returns data frame when non-valid setId", {
   expect_true("NULL" %in% class(getPivotDataSet(setId = "testSomeRandomName")))
 })
+
+test_that("error is provided on impossible combo of reshId and allData", {
+  expect_error(
+    getPivotDataSet(
+      setId = "pros_patient", registryName = "ablanor", session = list(),
+      singleRow = TRUE, reshId = NULL, allData = FALSE
+    )
+  )
+})
+
+# Test that require database infrastructure may be performed in test-getData
