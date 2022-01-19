a829943cd5fcbd19d330b1d38a2510c865f1771d
Kristina Skåre <kristina.skare@helse-bergen.no>
2022-01-19 14:28:14 +0100

    utlede variabler med indikator-spesifikke navn

--- R/kvalitetsindikatorer_ny.R
+++ R/kvalitetsindikatorer_ny.R
@@ -1,8 +1,8 @@
-#' Ablanor sine Kvalitetsindikatorer
+#' Ablanor sine kvalitetsindikatorer
 #'
 #' Funksjoner som lager 2 nye variabler for hver av kvalitetsindikatorene: Den
 #' første variabelen forteller hvorvidt forløpet er med i nevneren
-#' (datagrunnlaget) for den aktuelle indikatorer, den andre variabelnen
+#' (datagrunnlaget) for den aktuelle indikatorer, den andre variabelen
 #' forteller om forløpet er med i telleren (ja, nei, NA). Telleren er alltid
 #' NA dersom forløpet ikke er med i datagrunnlaget. Nevneren har suffix
 #' \emph{_data}.
@@ -10,34 +10,70 @@
 #' Følgende variabler er laget for de ulike funksjonene:
 #'
 #' \code{ki_komplikasjoner()}
-#' \itmeize{
-#' \item nevner \code{indikator_komplikasjon_data} (datagrunnlag) er \emph{ja}
+#' \itemize{
+#' \item nevneren \code{indikator_komplikasjon_data} (datagrunnlag) er \emph{ja}
 #'  for alle ferdigstilte skjema og \emph{nei} for ikke-ferdigstilte skjema.
-#' \item teller \code{indikator_komplikasjon} er \emph{ja} dersom
+#' \item telleren \code{indikator_komplikasjon} er \emph{ja} dersom
 #'  \code{komp_janei} er lik 1 og \emph{nei} dersom \code{komp_janei} er lik 0.
 #'   }
 #'
+#' \code{ki_dod_ny()}
+#' \itemize{
+#' \item nevneren \code{indikator_dod_data} (datagrunnlag) er \emph{nei}
+#'  for avdøde pasienter der enten \code{dato_pros} eller \code{deceased_date}
+#'  mangler, og \emph{nei} for levende pasienter der \code{dato_pros} mangler
+#'  eller det er færre enn \code{dager_innen} dager mellom prosedyren og
+#'  sensurdatoen (dette gjelder forløp nær datadump-dato, der eventuell
+#'  informasjon om død ikke har fått tid til å bli overført fra
+#'  folkeregisteret).
+#'  andre forløp er \code{indikator_dod_data} lik \emph{ja}.
+#' \item telleren \code{indikator_dod} er \emph{ja} dersom pasienten er død innen
+#' \code{dager_inne} dager etter prosedyren. \code{indikator_dod} er \emph{nei}
+#' dersom pasienten ikke er død, eller er død over \code{dager_innen} (ofte 30)
+#'  dager etter prosedyren. Dersom forløpet ikke er med i datagrunnlaget, så
+#'   er \code{indikator_dod} lik \emph{NA}.
+#' }
+#'
 #' @param df Ablanors koblete tabell (pros_patient_followup). Må inneholde
-#' utvalgte variabler, avhenger av hvilke indikator som skal regnes ut. F.eks.
+#' utvalgte variabler, avhengeig av hvilke indikator som skal regnes ut. F.eks.
 #' \code{ki_komplikasjoner()} trenger \emph{komp_janei} og \emph{pros_status}.
+#' @param dager_innen Numerisk. Default verdi er 30. Brukes som parameter i
+#' \code{ki_dod_ny()}, dersom antall dager mellom prosedyre og dødsfall er
+#' under dette sifferet så vil forløpet telle i indikatoren.
+#' @param dato_sensur Dato. Sensur-dato for overlevelse. Brukes som parameter i
+#' \code{ki_dod_ny()} Det tar inntil 30 dager for å overføre informasjon om død
+#' fra folkeregisteret til databasen. Forløp for nær dato for datadump bør ikke
+#' være med i datagrunnlaget for indikatoren.
 #' @name kvalitetsindikatorer_ny
 #' @aliases
 #' ki_komplikasjoner
+#' ki_dod_ny
 #'
 #' @examples
 #' x <- data.frame(
 #'         mceid = 1:5,
-#'         pros_status = c(1-, 0, 1, 1, 1),
+#'         pros_status = c(-1, 0, 1, 1, 1),
 #'         komp_janei = c(NA, 0, 1, 1, 1))
 #' ablanor::ki_komplikasjoner(df = x)
-#' Kvalitetsindikator for komplikasjoner
+#' x <- data.frame(
+#'   dato_pros = as.Date(c("2021-01-19",
+#'                         "2020-12-16",
+#'                         "2020-12-16"), format = "%Y-%m-%d"),
+#'   deceased = c(0, 1, 1),
+#'   deceased_date = as.Date(c(NA, "2022-01-12", "2020-12-16"),
+#'                           format = "%Y-%m-%d"))
+#' ablanor::ki_dod_ny(df = x,
+#'                    dato_sensur = as.Date("2022-01-20", format ="%Y-%m-%d"))
+
 NULL
 
 #' @rdname kvalitetsindikatorer_ny
 #' @export
 ki_komplikasjoner <- function(df) {
 
-    df %>%
+  stopifnot(c("pros_status", "komp_janei") %in% names(df))
+
+  df %>%
     dplyr::mutate(
 
       # datagrunnlag
@@ -57,7 +93,83 @@ ki_komplikasjoner <- function(df) {
         .data$indikator_komplikasjon_data == "nei" ~ NA_character_,
 
         FALSE ~ NA_character_)
-      )
+    )
+}
+
+#' @rdname kvalitetsindikatorer_ny
+#' @export
+ki_dod_ny <- function(df, dager_innen = 30, dato_sensur) {
+
+  stopifnot(c("dato_pros", "deceased_date" , "deceased") %in% names(df))
+
+  df %>%
+    dplyr::mutate(
+
+
+      dager_pros_sensur = dplyr::case_when(
+        # For avdøde:
+        .data$deceased == 1 ~ as.numeric(difftime(deceased_date,
+                                                  .data$dato_pros,
+                                                  units = "days")),
+        # For levende
+        .data$deceased == 0 ~ as.numeric(difftime(dato_sensur,
+                                                  .data$dato_pros,
+                                                  units = "days")),
+        TRUE ~ NA_real_),
+
+
+      # datagrunnlag : Kun pasienter som med over 30 dager mellom nedlasting av
+      # datadump (sensur) og prosedyre
+      indikator_dod_data = dplyr::case_when(
+
+        # alle døde, som har datoer, er med
+        .data$deceased == 1 &
+          !is.na(.data$dato_pros) &
+          !is.na(.data$deceased_date) ~ "ja",
+
+
+        # døde, som manger datoer, er  ikke med
+        .data$deceased == 1 &
+          (is.na(.data$dato_pros) | is.na(.data$deceased_date)) ~ "nei",
+
+
+        # levende med lang nok sensur
+        .data$deceased == 0 &
+          !is.na(.data$dato_pros) &
+          .data$dager_pros_sensur >= dager_innen ~ "ja",
+
+        # levende med for kort sensur, eller manglende dato for prosedyre
+        .data$deceased == 0 &
+          !is.na(.data$dato_pros) &
+          (.data$dager_pros_sensur < dager_innen |
+             is.na(.data$dager_pros_sensur)) ~ "nei",
+
+        # levende med for kort sensur, eller manglende dato for prosedyre
+        .data$deceased == 0 &
+          is.na(.data$dato_pros) ~ "nei",
+
+        TRUE ~ NA_character_),
+
+      indikator_dod = dplyr::case_when(
+        # Med i datagrunnlaget, død, død innen 30 dager
+        .data$indikator_dod_data == "ja" &
+          .data$dager_pros_sensur < dager_innen &
+          .data$deceased == 1 ~ "ja",
+
+        # Med i datagrunnlaget, død, over 30 dager
+        .data$indikator_dod_data == "ja" &
+          .data$dager_pros_sensur >= dager_innen &
+          .data$deceased == 1 ~ "nei",
+
+        # Med i datagrunnlaget, levende
+        .data$indikator_dod_data == "ja" &
+          .data$deceased == 0 ~ "nei",
+
+
+        .data$indikator_dod_data == "nei" ~ NA_character_,
+
+        FALSE ~ NA_character_)
+    )
 }
 
 
--- tests/testthat/test-kvalitetsindikatorer.R
+++ tests/testthat/test-kvalitetsindikatorer.R
@@ -10,27 +10,84 @@ test_that("indikator komplikasjoner fungerer", {
     c("pros_status",
       "komp_janei",
       "indikator_komplikasjon_data",
-      "indikator_komplikasjon_data"))
+      "indikator_komplikasjon"))
 
   # Alle som er med i datagrunnalget har ferdigstilt
   testthat::expect_true(all(
     x_out %>%
       dplyr::filter(.data$indikator_komplikasjon_data == "ja") %>%
-      dplyr::pull(.data$pros_staus) == 1))
+      dplyr::pull(.data$pros_status) == 1))
 
   # alle ferdigstilte er med i datagrunnlaget
   testthat::expect_true(all(
     x_out %>%
-      dplyr::filter(.data$pros_staus == 1) %>%
-      dplyr::pull(.data$indikator_komplikasjon_dat) == "ja"))
+      dplyr::filter(.data$pros_status == 1) %>%
+      dplyr::pull(.data$indikator_komplikasjon_data) == "ja"))
 
   # Ingen ikke-ferdigstilte er med i datagrunnlaget
   testthat::expect_true(all(
     x_out %>%
-      dplyr::filter(is.na(.data$pros_staus) | .data$pros_status != 1) %>%
-      dplyr::pull(.data$indikator_komplikasjon_dat) == "nei"))
+      dplyr::filter(is.na(.data$pros_status) | .data$pros_status != 1) %>%
+      dplyr::pull(.data$indikator_komplikasjon_data) == "nei"))
+
+})
+
+test_that("indikator død fungerer", {
+
+  dato_sensur = as.Date("2020-12-31", format = "%Y-%m-%d" )
+  x <- data.frame(dato_pros = as.Date(c(NA,
+                                        "2020-12-20",
+                                        "2020-05-16",
+                                        "2020-05-16",
+                                        "2020-05-16",
+                                        "2020-05-16",
+                                        "2020-05-16"), format = "%Y-%m-%d"),
+                  deceased = c(0, 0, 1, 0, 1, 1, 1),
+                  deceased_date = as.Date(c(NA,
+                                            NA,
+                                            "2020-05-16",
+                                            NA,
+                                            "2020-06-14",
+                                            "2021-01-15",
+                                            "2020-06-15")))
+
+  x_out <- ablanor::ki_dod_ny(df = x,
+                              dager_innen = 30,
+                              dato_sensur = dato_sensur)
 
 
+  testthat::expect_equal(
+    colnames(x_out),
+    c("dato_pros",
+      "deceased",
+      "deceased_date",
+      "dager_pros_sensur",
+      "indikator_dod_data",
+      "indikator_dod"))
 
+  # Minst 30 dager mellom prosedyre og sensur (dersom levende), eller død for
+  #  å være med i datagrunnlaget
+  testthat::expect_true(all(
+    x_out %>%
+      dplyr::filter(.data$indikator_dod_data == "ja",
+                    .data$deceased == 0) %>%
+      dplyr::pull(.data$dager_pros_sensur) >= 30))
 
+  # Døde, som har både prosedyre og dødsdato er med i datagrunnlaget
+  testthat::expect_true(all(
+    x_out %>%
+      dplyr::filter(.data$deceased == 1,
+                    !is.na(.data$dato_pros),
+                    !is.na(.data$deceased_date)) %>%
+      dplyr::pull(.data$.data$indikator_dod_data) == "ja"))
+
+testthat::expect_equal(
+  c("nei", "nei", rep("ja", 5)),
+  x_out %>% dplyr::pull(.data$indikator_dod_data)
+)
+
+testthat::expect_equal(
+  c(NA_character_, NA_character_, "ja", "nei", "ja", "nei", "nei"),
+  x_out %>% dplyr::pull(.data$indikator_dod)
+)
 })
