479203387da644358d883b63529d1419f20cb67e
Kristina Skåre <kristina.skare@helse-bergen.no>
2022-02-24 13:28:30 +0100

    added identity

--- R/some_new_stuff.R
+++ R/some_new_stuff.R
@@ -7,3 +7,10 @@
 # 2) New project  - from existing directory (in ablanor directory)
 
 # one more change
+
+
+# Did theese commands (terminal) in home-directory, to see if
+#      error message " load pubkey:::" disappears
+# $ eval "$(ssh-agent -a)"
+#  ssh-add ~/.ssh/id_rsa
+# RESULT:         Identity added: /home/rstudio/.ssh/id_rsa (skkris@docker)
