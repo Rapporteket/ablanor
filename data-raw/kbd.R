57375b8c3fd4b9e2250918c7925fea3be45331e2
Kristina Skåre <kristina.skare@helse-bergen.no>
2024-01-09 13:59:47 +0100

    ny kodebok etter release

--- data-raw/kbd.R
+++ data-raw/kbd.R
@@ -1,5 +1,5 @@
 ## code to prepare `kbd` dataset goes here
 
-kbd <-read.csv(file = "data-raw/klokeboken.csv", sep = ";")
+kbd <-read.csv(file = "data-raw/AblaNor_klokeboken_29.11.2023.csv", sep = ";")
 
 usethis::use_data(kbd, overwrite = TRUE)
