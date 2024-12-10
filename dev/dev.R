37f77110b6cfea1d2c707c375e30695071a62102
Arnfinn Hykkerud Steindal <arnfinn.hykkerud.steindal@helse-nord.no>
2024-12-10 14:08:46 +0100

    noe mer dev-greier

--- dev/dev.R
+++ dev/dev.R
@@ -1,13 +1,12 @@
-Sys.setenv(R_LIBCURL_SSL_REVOKE_BEST_EFFORT=TRUE)
-
-
-devtools::install("../rapbase/.", upgrade = FALSE)
-
+# Sys.setenv(R_LIBCURL_SSL_REVOKE_BEST_EFFORT=TRUE)
 # dekoding av database-dump
 # sship::dec("c://Users/ast046/Downloads/ablanor60c4616c4d0a.sql.gz__20241205_163003.tar.gz", keyfile = "p://.ssh/id_rsa")
 
+devtools::install("../rapbase/.", upgrade = FALSE)
+
 devtools::install(upgrade = FALSE)
 
+Sys.setlocale(locale = 'nb_NO.UTF-8')
 source("dev/sysSetenv.R")
 
 Sys.setenv(MYSQL_HOST="localhost") # for mobilt kontor
