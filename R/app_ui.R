656f09c1db79b8a0ca81edc4e66a6ceac65b4faf
Arnfinn Hykkerud Steindal <arnfinn.hykkerud.steindal@helse-nord.no>
2026-05-28 12:47:15 +0200

    Erstattet hideTab/showTab med removeTab/insertTab
    Faner kunne ligge i skjult i browser, men hentes frem med liten hack. Nå sendes ikke fane til browser for roller som ikke skal ha tilgang.
    
    Closes #86

--- R/app_ui.R
+++ R/app_ui.R
@@ -20,68 +20,6 @@ app_ui <- function() {
             rapbase::navbarWidgetInput("ablanorWidget", selectOrganization = TRUE)
           )
       ),
-
-      shiny::tabPanel(
-        "Utforsker",
-        shiny::fluidRow(
-          shiny::column(6, shiny::uiOutput("selectDataSet")),
-          shiny::column(6, shiny::uiOutput("selectVars"))
-        ),
-        shiny::fluidRow(
-          shiny::column(12, shiny::uiOutput("togglePivotSurvey"))
-        ),
-        shiny::fluidRow(
-          shiny::column(12, rpivotTable::rpivotTableOutput("pivotSurvey"))
-        )
-      ),
-
-      shiny::tabPanel(
-        "Kodebok",
-        shiny::sidebarLayout(
-          shiny::sidebarPanel(shiny::uiOutput("kbControl")),
-          shiny::mainPanel(shiny::htmlOutput("kbdData"))
-        )
-      ),
-
-      shiny::tabPanel(
-        title = "Datadump",
-        shiny::sidebarLayout(
-          shiny::sidebarPanel(
-            width = 4,
-            shiny::uiOutput(outputId = "selectDumpSet"),
-            shiny::dateRangeInput(
-              "dumpDateRange", "Velg periode:",
-              start = lubridate::ymd(Sys.Date()) - lubridate::years(1),
-              end = Sys.Date(), separator = "-",
-              weekstart = 1
-            ),
-            shiny::radioButtons("dumpFormat", "Velg filformat:",
-                         choices = c("csv", "xlsx-csv")),
-            shiny::downloadButton("dumpDownload", "Hent!")
-          ),
-          shiny::mainPanel(
-            shiny::htmlOutput("dataDumpInfo")
-          )
-        )
-      ),
-
-      shiny::tabPanel(
-        "Månedsrapporter",
-        shiny::sidebarLayout(
-          shiny::sidebarPanel(
-            shiny::radioButtons("formatReport",
-                                "Format for nedlasting",
-                                list(PDF = "pdf", HTML = "html"),
-                                inline = FALSE),
-            shiny::downloadButton("downloadReport", "Last ned!"),
-            width = 2
-          ),
-          shiny::mainPanel(
-            shiny::htmlOutput("maanedligRapport", inline = TRUE)
-          )
-        )
-      ),
-
       shiny::tabPanel(
         "Abonnement",
         shiny::sidebarPanel(
@@ -90,43 +28,6 @@ app_ui <- function() {
         ),
         shiny::mainPanel(
           rapbase::autoReportUI("ablanorSubscription")
-        )
-      ),
-
-      shiny::navbarMenu(
-        "Verktøy",
-        shiny::tabPanel(
-          "Utsending",
-          shiny::sidebarLayout(
-            shiny::sidebarPanel(
-              rapbase::autoReportFormatInput("ablanorDispatchment"),
-              rapbase::autoReportOrgInput("ablanorDispatchment"),
-              rapbase::autoReportInput("ablanorDispatchment")
-            ),
-            shiny::mainPanel(
-              rapbase::autoReportUI("ablanorDispatchment")
-            )
-          )
-        ),
-
-        shiny::tabPanel(
-          "Eksport",
-          shiny::sidebarLayout(
-            shiny::sidebarPanel(
-              rapbase::exportUCInput("ablanorExport")
-            ),
-            shiny::mainPanel(
-              rapbase::exportGuideUI("ablanorExportGuide")
-            )
-          )
-        ),
-
-        shiny::tabPanel(
-          "Bruksstatistikk",
-          shiny::sidebarLayout(
-            shiny::sidebarPanel(rapbase::statsInput("ablanorStats")),
-            shiny::mainPanel(rapbase::statsUI("ablanorStats"))
-          )
         )
       )
     )
