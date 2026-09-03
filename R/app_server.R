2ccc3efd5520b48283f082444bc960d8da3720df
Mary Gerina Autenried <mary.gerina.autenried@helse-bergen.no>
2026-09-03 14:04:54 +0200

    Kun nedlasting av månedsrapport i pdf-format

--- R/app_server.R
+++ R/app_server.R
@@ -90,11 +90,9 @@ app_server <- function(input, output, session) {
           "Månedsrapporter",
           shiny::sidebarLayout(
             shiny::sidebarPanel(
-              shiny::radioButtons("formatReport",
-                "Format for nedlasting",
-                list(PDF = "pdf", HTML = "html"),
-                inline = FALSE),
-              shiny::downloadButton("downloadReport", "Last ned!"),
+              style = "position:fixed;width:130px;",
+              h5("Last ned rapporten (pdf)"),
+              shiny::downloadButton("downloadReport", "Hent!"),
               width = 2
             ),
             shiny::mainPanel(
