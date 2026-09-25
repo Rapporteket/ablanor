63f3bd9446b78eaa00eed06e3fd27fbf75e17fd0
Mary Gerina Autenried <mary.gerina.autenried@helse-bergen.no>
2026-09-25 08:50:37 +0200

    Prøver med nyere dato

--- data-raw/kbd.R
+++ data-raw/kbd.R
@@ -1,5 +1,5 @@
 ## code to prepare `kbd` dataset goes here
 
-kbd <-read.csv(file = "data-raw/AblaNor_klokeboken_16.09.2026.csv", sep = ";")
+kbd <-read.csv(file = "data-raw/AblaNor_klokeboken_24.09.2026.csv", sep = ";")
 
 usethis::use_data(kbd, overwrite = TRUE)
